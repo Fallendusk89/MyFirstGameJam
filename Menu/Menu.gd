2c232844c85f19214b606134c136e4ff46949b70
Fallendusk89 <121059396+Fallendusk89@users.noreply.github.com>
2023-07-29 09:06:08 -0600

    Add files via upload

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Level_1/Level_1.tscn")
+	get_tree().change_scene_to_file("res://Menu/Made_by.tscn")
 
 func _on_options_pressed():
 	pass # Replace with function body.
